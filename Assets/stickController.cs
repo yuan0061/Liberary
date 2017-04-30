401ec29f2b20a781ad3426ca090568a5e179d3aa
陳德元 <yuan0061@gmail.com>
2017-05-01 00:09:32 +0800

    Hello Liberary

---
+++ Assets/stickController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stickController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnColliderEnter2D(Collider collider){
+		Debug.Log ("enter");
+	}
+
+
+	void OnTriggerEnter2D(Collider2D collider){
+		Debug.Log (collider.transform.position.y);
+		if (collider.transform.position.y > this.transform.position.y) {
+			Debug.Log ("come from up");
+			//collider.transform.TransformVector;
+		}
+	}
+}

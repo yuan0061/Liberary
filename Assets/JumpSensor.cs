6e7f2ad30fd2e82d3c90899623299abb46e17ff2
sophiawang95 <jiwang95@gmail.com>
2017-05-01 22:40:23 +0800

    add some walk

---
+++ Assets/JumpSensor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpSensor : MonoBehaviour {
+    private int TouchedCount = 0;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        TouchedCount++;
+        //Debug.Log("in");
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        TouchedCount--;
+        //Debug.Log("out");
+    }
+
+    public bool IsCanJump()
+    {
+        return TouchedCount > 0;
+    }
+
+}
--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class playerController : MonoBehaviour {
+    public float forceValue;
+    public float maxSpeed;
+    public float decreasingSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public JumpSensor JumpSensor;
+    public float JumpSpeed;
+
+
+    private Rigidbody2D rigidbody2D = null;
+    // Use this for initialization
+    void Start () {
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
 
 	float speed = 10;
 	
@@ -26,6 +34,52 @@ public class playerController : MonoBehaviour {
 			this.transform.position += new Vector3 (speed * Time.deltaTime, 0, 0);
 		}
 
+        Vector2 force = Vector2.zero;
+        if (Input.GetKey(KeyCode.Space) && JumpSensor.IsCanJump())
+        {
+            force.y = JumpSpeed;
+            //Debug.Log("Jump");
+        }
+        if (force != Vector2.zero)
+        {
+            rigidbody2D.AddForce(force);
+        }
 
-	}
+        //add other walking way
+        if (Input.GetKey(KeyCode.RightArrow)|| Input.GetKey(KeyCode.LeftArrow))
+        {
+            
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                force += new Vector2(forceValue, 0);
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                force += new Vector2(-forceValue, 0);
+                //gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+            if (force != Vector2.zero)
+            {
+                rigidbody2D.AddForce(force);
+                float speed = rigidbody2D.velocity.magnitude;
+                if (speed > maxSpeed)
+                {
+                    speed = maxSpeed;
+                }
+                rigidbody2D.velocity = rigidbody2D.velocity.normalized * speed;
+            }
+            else
+            {
+                float speed = rigidbody2D.velocity.magnitude;
+                speed -= decreasingSpeed * Time.deltaTime;
+                if (speed < 0)
+                {
+                    speed = 0;
+                }
+                rigidbody2D.velocity = rigidbody2D.velocity.normalized * speed;
+            }
+
+        }
+
+    }
 }

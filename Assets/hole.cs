d90d50df61327fada7b624480c5a9b80ff51d65e
陳德元 <yuan0061@gmail.com>
2017-06-01 12:43:30 +0800

    新增場景：印刷廠 ＆ 劍玉

---
+++ Assets/hole.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hole : MonoBehaviour {
+	
+	Vector3 point;
+	float x;
+	float y;
+	float z;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+		
+	}
+	void OnMouseDrag()
+	{
+		x = Input.mousePosition.x;
+		y = Input.mousePosition.y;
+
+
+		point = Camera.main.ScreenToWorldPoint (new Vector3 (x, y, 1f));
+		gameObject.transform.position = point;
+
+
+	}
+}
---
+++ Assets/lineScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineScript : MonoBehaviour {
+
+	public LineRenderer line;
+
+
+	// Use this for initialization
+	void Start () {
+		LineSetup ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		LineUpdate ();
+	}
+
+	void LineSetup(){
+		line.SetPosition (0, line.transform.position);
+		line.widthMultiplier = 0.05f;
+		line.sortingOrder = 1;
+	}
+
+	void LineUpdate(){
+		/*Vector2 catapultToProjectile = transform.position - line.transform.position;
+		leftCatapultToProjectile.direction = catapultToProjectile;
+		Vector3 holdPoint = leftCatapultToProjectile.GetPoint (catapultToProjectile.magnitude);
+		catapultLineFront.SetPosition (1, holdPoint);
+		catapultLineBack.SetPosition (1, holdPoint);*/
+
+		line.SetPosition (1, transform.position);
+	}
+}

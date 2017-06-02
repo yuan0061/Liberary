524b6d3cd7d32266377c35958f28ed73004c5b76
陳德元 <yuan0061@gmail.com>
2017-06-02 14:24:14 +0800

    Success print 1%

--- Assets/script/winOrLose.cs
+++ Assets/script/winOrLose.cs
@@ -10,16 +10,24 @@ public class winOrLose : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	
+
     void Update () {
+		//Debug.Log (Input.mousePosition);
+		GameObject gb=GameObject.FindWithTag("one");
+		//Debug.Log (gb.name);
+		float x=gb.transform.position.x;
+		float y = gb.transform.position.y;
+		float numberX = transform.position.x;
+		float numberY = transform.position.y;
+
+		if (x>numberX-0.5 & x<numberX+0.5 & y>numberY-0.5 & y<numberY+0.5) {
+			gb.transform.position = transform.position;
+		
+		}
 		
 	}
 	
 
 
-	void OnTriggerEnter2D(Collider2D other){
-			
-		Debug.Log ("撞到了");
 
-	}
 }

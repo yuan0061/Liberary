d3a916d7931b2b117ce018be8e9ed1848d1b56be
陳德元(Debbie Chen) <yuan0061@gmail.com>
2017-06-11 11:53:00 +0800

    人會生成＆對話＆加傳播率

--- Assets/transport.cs
+++ Assets/transport.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 
 
 public class transport : MonoBehaviour {
-	public GameObject persent;
+	public GameObject persent1;
+	public GameObject persent3;
+	public GameObject persent5;
+	public GameObject g1;
+	public GameObject g2;
+	public GameObject g3;
+	public GameObject b1;
+	public GameObject b2;
+	public GameObject b3;
 	public comManager commanager;
+	private float ran;
+	private float ran2;
 
 	bool isClicked=false;
 	// Use this for initialization
@@ -18,8 +28,36 @@ public class transport : MonoBehaviour {
 	void Update () {
 		
 		if(isClicked==true && Input.GetKeyDown(KeyCode.Return)){
-			persent.SetActive (true);
-			commanager.AddScore (5);
+			ran = Random.Range (0f, 1f);
+			Debug.Log (ran);
+			if (ran < 0.33) {
+				persent1.SetActive (true);
+				commanager.AddScore (1);
+				if (ran < 0.165) {
+					g1.SetActive (true);
+				} else {
+					b1.SetActive (true);
+				}
+			} else if (ran >= 0.33 && ran <= 0.67) {
+				persent3.SetActive (true);
+				commanager.AddScore (3);
+				if (ran < 0.5) {
+					g2.SetActive (true);
+				} else {
+					b2.SetActive (true);
+				}
+			} else {
+				persent5.SetActive (true);
+				commanager.AddScore (5);
+				if (ran < 0.83) {
+					g3.SetActive (true);
+				} else {
+					b3.SetActive (true);
+				}
+			}
+			Invoke ("noActive", 1);
+			Invoke ("active", 1);
+
 			//Debug.Log ("456");
 		}
 
@@ -30,6 +68,24 @@ public class transport : MonoBehaviour {
 		isClicked = true;
 
 	}
+	void noActive(){
+		this.gameObject.SetActive (false);
+	}
 
+	void active(){
+		ran2 = Random.Range (30f, 100f);
+		this.gameObject.transform.position = new Vector3(ran2, this.gameObject.transform.position.y, 0);
+		this.gameObject.SetActive (true);
+		isClicked=false;
+		persent1.SetActive (false);
+		persent3.SetActive (false);
+		persent5.SetActive (false);
+		g1.SetActive (false);
+		g2.SetActive (false);
+		g3.SetActive (false);
+		b1.SetActive (false);
+		b2.SetActive (false);
+		b3.SetActive (false);
+	}
 
 }
